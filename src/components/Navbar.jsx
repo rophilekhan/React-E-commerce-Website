b345891fe7d36149701920ea07a6d8eb6a370295
Rophile Ahmed Khan <rophile.khan@gmail.com>
2024-09-16 05:14:14 +0500

    both navbar completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,54 @@
 import React from "react";
 import Logo from "../assets/logo.png";
 import { IoMdSearch } from "react-icons/io";
-import { FaCartShopping } from "react-icons/fa6";
+import { FaCaretDown, FaCartShopping } from "react-icons/fa6";
 import DarkMode from "./DarkMode";
+
+const Menu = [
+  {
+    id: 1,
+    name: "Home",
+    link: "/#",
+  },
+  {
+    id: 2,
+    name: "Top Rated",
+    link: "/services",
+  },
+  {
+    id: 3,
+    name: "Kids Wear",
+    link: "/#",
+  },
+  {
+    id: 4,
+    name: "Mens Wear",
+    link: "/#",
+  },
+  {
+    id: 5,
+    name: "Electronics",
+    link: "/#",
+  },
+];
+
+const DropdownLinks = [
+  {
+    id: 1,
+    name: "Trending Products",
+    link: "/#"
+  },
+  {
+    id: 2,
+    name: "Best Selling",
+    link: "/#"
+  },
+  {
+    id: 3,
+    name: "Top Rated",
+    link: "/#"
+  },
+] 
 
 const Navbar = () => {
   return (
@@ -26,32 +72,65 @@ const Navbar = () => {
                          group-hover:w-[300px]
                          transition-all duration-300
                          rounded-full border border-gray-300 px-2 py-1 focus:outline-none
-                         focus:border-1 focus:border-primary"
+                         focus:border-1 focus:border-primary dark:border-gray-500 dark:bg-gray-800"
               />
               <IoMdSearch className="text-gray-500 group-hover:text-primary absolute top-1/2 -translate-y-1/2 right-3" />
             </div>
-          {/* Order Button */}
-          <button
-            onClick={() => alert("Ordering not available yet")}
-            className="bg-gradient-to-r from-primary to-amber-600 transition-all duration-0
+            {/* Order Button */}
+            <button
+              onClick={() => alert("Ordering not available yet")}
+              className="bg-gradient-to-r from-primary to-amber-600 transition-all duration-0
              text-white py-1 px-4 rounded-full flex items-center gap-3 group"
-          >
-            <span
-            className="group-hover:block hidden transition-all duration-200"
-            >Order</span>
-            <FaCartShopping
-            className="text-xl text-white drop-shadow-sm cursor-pointer"
-            />
-          </button>
-          {/* theme changer button */}
-          <div>
-            <DarkMode />
-          </div>
+            >
+              <span className="group-hover:block hidden transition-all duration-200">
+                Order
+              </span>
+              <FaCartShopping className="text-xl text-white drop-shadow-sm cursor-pointer" />
+            </button>
+            {/* theme changer button */}
+            <div>
+              <DarkMode />
+            </div>
           </div>
         </div>
       </div>
       {/* lower Navbar */}
-      <div></div>
+      <div className="flex justify-center">
+        <ul className="sm:flex hidden items-center gap-4">
+          {Menu.map((data) => (
+            <li key={data.id}>
+              <a
+                href={data.link}
+                className="inline-block px-4 hover:text-primary duration-200"
+              >
+                {data.name}
+              </a>
+            </li>
+          ))}
+          {/* simple DropDown and links */}
+          <li className="group relative cursor-pointer">
+            <a href="#" className="flex items-center gap-[2px] py-2">
+              Trending Products
+              <span>
+                <FaCaretDown className="transition-all duration-200 group-hover:rotate-180" />
+              </span>
+            </a>
+            <div className="absolute z-[9999] hidden group-hover:block w-[150px] rounded-md bg-white p-2 text-black shadow-md">
+              <ul>
+                {DropdownLinks.map((data)=>(
+                  <li key={data.id}>
+                    <a href={data.link}
+                    className="inline-block w-full rounded-md p-2 hover:bg-primary/20"
+                    >{data.name}
+                    </a>
+                  </li>
+                ))
+                }
+              </ul>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
